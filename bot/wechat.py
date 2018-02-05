c294127c6a3405fedb27f2cb8536128e273f8fd2
zhangtianchen <zhangtianchen@daoxila>
2018-02-06 00:26:30 +0800

    creat env

--- bot/wechat.py
+++ bot/wechat.py
@@ -5,41 +5,58 @@ from wxpy import *
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
-chatbot = ChatBot("deepThought")
-chatbot.set_trainer(ChatterBotCorpusTrainer)
-chatbot.train("chatterbot.wcorpus.chinese")  # 使用该库的中文语料库
-chatbot.set_trainer(ListTrainer)
 
-bot = Bot()  # 用于接入微信的机器人
-friend = bot.friends().search('琛')[0]
-friends = bot.friends()
-group = bot.groups("群") # 进行测试的群
+class xiaobei:
+    def __init__(self):
+        self.chatbot = ChatBot("deepThought")
+        self.chatbot.set_trainer(ChatterBotCorpusTrainer)
+        self.chatbot.train("chatterbot.wcorpus.chinese")  # 使用该库的中文语料库
+        self.chatbot.set_trainer(ListTrainer)
+
+        # bot = Bot()  # 用于接入微信的机器人
+        # self.friend = self.bot.friends().search('张小贝')[0]
+        self.friends = self.bot.friends()
+        self.group = self.bot.groups("群")  # 进行测试的群
+
+    # @self.bot.register(self.group)
+    def reply_my_friend(self, msg):
+        print(msg)
+        if "@张小贝" in msg.text:
+            return self.reply_message(msg.text)  # 使用机器人进行自动回复
+        return ""
+
+    def reply_message(self, msg):
+        flag = "学习"
+        if msg.find(flag) == 0:
+            return self.learn_response(msg)
+        respmsg = self.chatbot.get_response(msg)  # 使用机器人进行自动回复
+        print(self.chatbot.get_response(msg))
+        return respmsg.text
+
+    def learn_response(self, msg):
+        arr = msg.split("&")
+        if len(arr) == 3:
+            question = arr[1]
+            answer = arr[2]
+            self.train(question, answer)
+            return "好的，我学习了"
+        else:
+            return "我学习不聊。格式不对。格式类似于 学习&你吃饭了吗&吃了"
+
+    def train(self, question, answer):
+        self.chatbot.train([question, answer])
 
 
-@bot.register(friends)
+bot = Bot()
+friend = bot.friends().search('张小贝')[0]
+xiaobei = xiaobei()
+
+
+@bot.register(friend)
 def reply_my_friend(msg):
     print(msg)
-    return reply_message(msg.text)  # 使用机器人进行自动回复
+    return xiaobei.reply_message(msg.text)  # 使用机器人进行自动回复
 
 
-@bot.register(group)
-def reply_my_friend(msg):
-    print(msg)
-    if "@张小贝" in msg.text:
-        return reply_message(msg.text)  # 使用机器人进行自动回复
-    return ""
-
-
-def reply_message(msg):
-    respmsg = chatbot.get_response(msg)  # 使用机器人进行自动回复
-    print(chatbot.get_response(msg))
-    return respmsg.text
-
-
-def train(question, answer):
-    print("old： " + chatbot.get_response(question).text)
-    chatbot.train([question, answer])
-    print("new： " + chatbot.get_response(question).text)
-
-# 堵塞线程，并进入 Python 命令行
-embed()
+if __name__ == '__main__':
+    embed()
